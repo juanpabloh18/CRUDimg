cadc86f5a40af4ebb8f712302b248393ab7e7478
Juan Pablo Hurtado Agudelo <123783172+juanpabloh18@users.noreply.github.com>
2024-05-16 17:06:27 -0500

    En el componente Home ya se visualizan las imagenes almacendas en el storage

--- src/Paginas/Home.jsx
+++ src/Paginas/Home.jsx
@@ -1,17 +1,60 @@
 import { useState, useEffect } from "react";
 import { db } from "../firebase/firebase";
-import { Button, Card, Grid, Container, Image, GridColumn, CardDescription, Item, Modal } from "semantic-ui-react";
+import { Button, Card, Grid, Container, Image, GridColumn, CardDescription, Item, Modal, List } from "semantic-ui-react";
 import { useNavigate } from "react-router-dom";
 import { collection, deleteDoc, doc, onSnapshot } from "firebase/firestore";
 import ModalComponente from "../Componentes/ModalComponente";
 
 const Home = () => {
-   
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setLoading(true);
+        const unsub = onSnapshot(
+            collection(db, "users"),
+            (snapshot) => {
+                let list = [];
+                snapshot.docs.forEach((doc) => {
+                    list.push({ id: doc.id, ...doc.data() });
+                });
+                setUsers(list);
+                setLoading(false)
+            }, (error) => {
+                console.log(error);
+            }
+        );
+
+        return () => {
+            unsub();
+        };
+    }, []);
     return (
-        <>
-    <h1>Este es el inicio</h1>
-        </>
+        <Container>
+            <Card.Group>
+                <Grid columns={1} stackable>
+                    {users && users.map((item) => (
+                        <Grid.Column>
+                            <Card key={item.id}>
+                                <Card.Content>
+                                    <Image
+                                        src={item.img}
+                                        size="medium"
+                                        style={{
+                                            height: "150px",
+                                            width: "150px",
+                                            borderRadius:"50",
+                                        }}
+                                    />
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                    ))}
+                </Grid>
+            </Card.Group>
+        </Container>
     )
-}
+};
 
 export default Home;
